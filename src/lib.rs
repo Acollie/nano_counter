26c8aaa1c0f96e4e6ac2903970ff2cdff718955f
Alexcollie <cola@pop-os.localdomain>
2022-02-02 13:54:54 +0000

    Fixing string issues and lifetime changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,41 @@
 use std::collections::{HashMap};
 
-struct Word{
-    key:String,
-    count:i32,
+struct Word<'a>{
+    key: &'a str,
+    count: i32,
 }
-struct Word_count{
-    words: HashMap<String, Word>,
+struct WordCount<'a> {
+    words: HashMap<&'a str,  Word<'a> >,
 }
 
 
-impl Word_count {
-    fn add_word(&mut self, key: String){
+impl WordCount <'static>{
+    
+    fn add_word(&mut self, key_dic: &'static str){
 
-        if self.words.contains_key(&key){
-            let word_target = self.words.get_mut(&key).unwrap();
+        if self.words.contains_key(&key_dic){
+            let word_target = self.words.get_mut(key_dic).unwrap();
             word_target.count +=1;
         }else{
-            self.words.insert(key.clone(), Word{ key: key.clone(), count: 1 });
+            self.words.insert(key_dic, Word { key: key_dic, count: 1 });
         }
     }
-    fn get_word_count(&self, key: String) ->i32{
+    fn get_word_count(&self, key: &str) ->i32{
+        match self.words.get(&key){
+            Some(value)=>return value.count,
+            None => return 0
+        }
+
         return self.words.get(&key).unwrap().count;
 
     }
-    fn remove_word(&mut self,key:String){
-        let key_string = key.to_string();
-        self.words.remove(&key_string);
+    fn remove_word(&mut self,key:&str){
+        self.words.remove(key);
     }
     fn most_common_words(&self) ->Vec<Word>{
 
         let mut most_common_words :Vec<Word>= Vec::new();
-        // let words = Vec::from_iter(self.words);
+
         let foo = &self.words;
         println!("{:?}",self.words.get("foo").unwrap().key);
         // let sorted_keys = words.sort_by(|a,b| b.count.cmp(&a.count));
@@ -49,32 +54,33 @@ mod tests {
 
     #[test]
     fn add_word() {
-        let mut words = Word_count{
+        let mut words = WordCount{
             words: Default::default()
         };
-        words.add_word((&"Test").to_string());
-        assert_eq!(words.get_word_count((&"Test").to_string()), 1);
+        words.add_word("Test");
+        assert_eq!(words.get_word_count("Test"), 1);
     }
 
     #[test]
     fn add_word_count() {
-        let mut words = Word_count{
+        let mut words = WordCount{
             words: Default::default()
         };
-        words.add_word((&"Test").to_string());
-        words.add_word((&"Test1").to_string());
-        words.add_word((&"Test").to_string());
-        assert_eq!(words.get_word_count((&"Test").to_string()), 2);
+        words.add_word("Test");
+        words.add_word("Test1");
+        words.add_word("Test");
+        assert_eq!(words.get_word_count("Test"), 2);
     }
 
     #[test]
     fn remove_word() {
-        let mut words = Word_count{
+        let mut words = WordCount{
             words: Default::default()
         };
-        words.add_word((&"Test").to_string());
-        words.remove_word((&"Test").to_string());
-        assert_eq!(words.get_word_count((&"Test").to_string()), 0);
+        words.add_word("Test");
+        words.remove_word("Test");
+        println!("----->{}",words.get_word_count("Test"));
+        assert_eq!(words.get_word_count("Test"), 0);
     }
     #[test]
     fn count_words(){
